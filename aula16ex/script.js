9b42f73b3dbebf2dc64bfd0d84b0a9000158d835
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-06-07 17:28:24 -0300

    finalizando curso

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -1,16 +1,15 @@
-var numeros = [1, 2]
+var numeros = []
 function adicionar(){
     var numero = document.getElementById('num')
     var select = document.getElementById('select')
     if(numero.value.length == 0){
         window.alert('digite um numero')
     }
-    else if(numero.value <= 100 && numero.value >= 1){
+    else if(numero.value <= 100 && numero.value >= 0){
         var num_processado = Number(numero.value)
         if(numeros.indexOf(num_processado) == -1){
             numeros.push(num_processado)
-            window.alert(`numero ${num_processado} adicionado`)
-            select.innerHTML += `<option>`
+            select.innerHTML += `<option>${num_processado}</option>`
         }
         else{
             window.alert(`O numero ${num_processado} já foi adicionado`)
@@ -18,5 +17,34 @@ function adicionar(){
     }
     else{
         window.alert('numero inválido')
+    }
+    numero.value = ''
+    numero.focus()
+}
+
+function analisar(){
+    if(numeros.length == 0){
+        window.alert('Adicione numeros primeiro')
+    }
+    else{
+        var resultado = document.getElementById('resultado')
+        var resultado_title = document.getElementById('resultado-title')
+        resultado_title.style.display = 'block'
+        resultado.innerHTML = ''
+
+        resultado.innerHTML += `<p>Total de numeros cadastrados: ${numeros.length}</p>`
+
+        resultado.innerHTML += `<p>O maior numero informado foi: ${Math.max(...numeros)}</p>`
+
+        resultado.innerHTML += `<p>O menor numero informado foi: ${Math.min(...numeros)}</p>`
+
+        var soma = 0
+        for(var c = 0; c < numeros.length; c++){
+            soma += numeros[c]
+        }
+        resultado.innerHTML += `<p>A soma de todos os valores é: ${soma}</p>`
+
+        var media = soma/numeros.length
+        resultado.innerHTML += `<p>a média dos valores é: ${media}</p>`
     }
 }
---
+++ aula17/script.js
@@ -0,0 +1,5 @@
+let amigo = {nome:'José', sexo: 'M', peso: 80.2, engordar(p=0){
+    this.peso += p
+}}
+
+console.log(amigo)
